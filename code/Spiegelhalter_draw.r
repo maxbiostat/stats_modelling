4400487b2591dca7ad39d2de000d8a31af3b2aa0
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2023-03-01 14:31:16 -0300

    Book draw

---
+++ code/Spiegelhalter_draw.r
@@ -0,0 +1,7 @@
+Nomes <- c("Amanda Perez", "Cristiano Leal", "Lucas Westfal",
+           "Eduardo Adame", "Felipe Lamarca", "Luis Fernando",
+           "Pedro Lima")
+
+set.seed(666)
+
+Nomes[sample(seq_along(Nomes), 1)]
