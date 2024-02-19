a919bc4b7a582641abbd6f4a3cf9cb10181a6224
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2024-02-19 10:59:32 -0300

    clean up

--- code/Spiegelhalter_draw.r
+++
@@ -1,7 +0,0 @@
-Nomes <- c("Amanda Perez", "Cristiano Leal", "Lucas Westfal",
-           "Eduardo Adame", "Felipe Lamarca", "Luis Fernando",
-           "Pedro Lima")
-
-set.seed(666)
-
-Nomes[sample(seq_along(Nomes), 1)]
