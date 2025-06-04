c2572b296eb8532d1f1e88504824883e3af3d30b
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2025-06-04 10:58:26 -0300

    some updates to teen smoking analysis

--- code/adolescent_smoking.r
+++ code/adolescent_smoking.r
@@ -1,18 +1,58 @@
-TeenSmoking <- read.table("smoke_pub.dat", 
-           header = TRUE)
+library(rstanarm)
+library(posterior)
+
+TeenSmoking <- read.csv("../data/edgy_teens.csv")
 perWave <- split(TeenSmoking, TeenSmoking$newid)
 
 barplot(table(unlist(lapply(perWave, nrow))))
 
 summary(TeenSmoking)
 
-library(rstanarm)
+barplot(table(TeenSmoking$sex.1.F.))
+
+barplot(table(TeenSmoking$parsmk))
+
+round(prop.table(table(TeenSmoking$sex.1.F., TeenSmoking$smkreg)), 2)
+round(prop.table(table(TeenSmoking$parsmk, TeenSmoking$smkreg)), 2)
+
+
+################
+### Modelos "simples"
+f1 <- "smkreg ~ sex.1.F. + parsmk + wave"
+m1 <- glm(f1,
+          family = "binomial",
+          data = TeenSmoking)
+summary(m1)
+
+f2 <- "smkreg ~ parsmk + sex.1.F. * wave"
+m2 <- glm(f2,
+          family = "binomial",
+          data = TeenSmoking)
+summary(m2)
+
+
+################
+### Modelos multinível
 
 options(mc.cores = parallel::detectCores())
 
-mformula <- "smkreg ~ sex.1.F. + parsmk + wave + (1|newid)"
-mod1 <- stan_glmer(mformula,
+mlf1 <- "smkreg ~ sex.1.F. + parsmk + wave + (1|newid)"
+mod1 <- stan_glmer(mlf1,
                  family = "binomial",
                  data = TeenSmoking)
 
-as_draws(mod1)
+mod1
+
+re1 <- ranef(mod1)
+lattice::dotplot(re1, scales = list(x =list(relation = 'free')))
+
+
+mlf2 <- "smkreg ~  parsmk + (1|newid) + (1 + wave|sex.1.F.)"
+mod2 <- stan_glmer(mlf2,
+                   family = "binomial",
+                   data = TeenSmoking)
+
+mod2
+
+re2 <- ranef(mod2)
+lattice::dotplot(re2, scales = list(x =list(relation = 'free')))
