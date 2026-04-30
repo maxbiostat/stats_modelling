4aeb0a07622c333540d8139ca54c8052d1d49f68
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2026-04-30 11:30:25 -0300

    fiddling with lecture stuff

--- em_aula/licao_4_em_aula.r
+++ em_aula/licao_4_em_aula.r
@@ -1,3 +1,11 @@
 library(tidyverse)
-kidiq <- read_csv("kidiq.csv")
+kidiq <- read_csv("../data/kidiq.csv")
 head(kidiq)
+
+kidiq <- kidiq %>% mutate(mom_iq_c = mom_iq - mean(mom_iq))
+
+mod <- lm(kid_score ~ mom_iq*mom_hs, data = kidiq)
+modc <- lm(kid_score ~ mom_iq_c*mom_hs, data = kidiq)
+
+summary(mod)
+summary(modc)
